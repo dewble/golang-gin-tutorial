35cd3e13a7ef661bbefcd821069269750ee4db06
dewble <dewble62@gmail.com>
2021-09-24 00:14:14 +0900

    add models/article.go for get article list

--- board/models/article.go
+++ board/models/article.go
@@ -5,3 +5,13 @@ type article struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
+
+var articleList = []article{
+	article{ID: 1, Title: "Title1", Content: "Content 1"},
+	article{ID: 2, Title: "Title2", Content: "Content 2"},
+}
+
+// return a list of all the articles
+func getAllArticles() []article {
+	return articleList
+}
--- board/models/article_test.go
+++ board/models/article_test.go
@@ -1,1 +1,24 @@
 package models
+
+import "testing"
+
+// Test for getAllarticles
+func TestGetAllArticles(t *testing.T) {
+	alist := getAllArticles()
+
+	// 1. 가져온 게시글 리스트와 전역 변수 게시글 리스트의 길이가 동일한지 확인
+	// 2. 각 게시글이 동일한지 확인하기 위해 반복문 사용. 1,2 둘중 어느것이라도 통과못할 경우 테스트 실패
+	if len(alist) != len(articleList) {
+		t.Fail()
+	}
+
+	for i, v := range alist {
+		if v.Content != articleList[i].Content ||
+			v.ID != articleList[i].ID ||
+			v.Title != articleList[i].Title {
+
+			t.Fail()
+			break
+		}
+	}
+}
